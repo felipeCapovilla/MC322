6c5366341f76ed2cd30615c37f5495927244d151
felipeCapovilla <felipe.pavanello.capovilla@gmail.com>
2025-03-21 12:07:34 -0300

    Comentarios feitos em RoboExplorador

--- lab02/src/RoboVoadorExplorador.java
+++ lab02/src/RoboVoadorExplorador.java
@@ -12,7 +12,8 @@ public class RoboVoadorExplorador extends RoboAereo {
         
         super(nome,posicaoX,posicaoY,direcao,altitude,altitude_max); //Inicializa as variaveis da classe herdada.
         
-        this.velocidade_max = velocidade_max; //Atribui vel. maxima.
+        this.velocidade_max = velocidade_max;
+        
         //Inicia o robo como 'inativo'.
         this.pressao_atual = 0;
         this.temperatura_atual =0;
@@ -21,63 +22,111 @@ public class RoboVoadorExplorador extends RoboAereo {
         this.em_missao = false;
     }
 
+    /** 
+     * Inicia a atividade do robo.
+     * @param pressao_atual Pressao atual do ambiente em que esta.
+     * @param temperatura_atual Temperatura do ambiente que esta.
+     * @param velocidade_atual Velocidade atual.
+     * @param planeta Qual planeta esta o robo.
+    */
     protected void iniciar_exploracao(int pressao_atual, int temperatura_atual, int velocidade_atual, String planeta){
-        if(velocidade_atual > this.velocidade_max){ //Verifica se a velocidade setada respeita os limites do robo.
-            throw new IllegalArgumentException("A velocidade do Robo não pode ultrapassar"+this.velocidade_max+"m/s"); //Se nao for permitido, lança erro.
+
+        if(velocidade_atual > this.velocidade_max){ //Verifica se velocidade respeita limites do robo.
+            throw new IllegalArgumentException("A velocidade do Robo não pode ultrapassar"+this.velocidade_max+"m/s"); //Se nao: lança erro.
         }
-        this.velocidade_atual = velocidade_atual; //Caso contrario, seta o valor.
+        this.velocidade_atual = velocidade_atual; //Se sim: seta o valor.
         
         if(temperatura_atual < 0){ //Verifica se a temperatura é plausivel.
-            throw new IllegalArgumentException("A temperatura nao pode ser menor que 0K"); //Se nao, lança erro.
+            throw new IllegalArgumentException("A temperatura nao pode ser menor que 0K"); //Se nao: lança erro.
         }
-        this.temperatura_atual = temperatura_atual; //Se sim, seta valor.
+        this.temperatura_atual = temperatura_atual; //Se sim: seta valor.
 
-        //Atualiza atributos do robo.
         this.em_missao = true; 
         this.pressao_atual = pressao_atual;
         this.planeta_atual = planeta;
     }
 
-    protected int set_temperatura(int nova_temperatura){ //Altera temperatura atual do robo.
+
+    /**
+     * Altera a temperatura atual percebida pelo robo.
+     * @param nova_temperatura Nova temperatura a ser setada.
+     */
+    protected int set_temperatura(int nova_temperatura){ 
         if(nova_temperatura < 0){ //Verifica se a temperatura é plausivel.
-            throw new IllegalArgumentException("A temperatura nao pode ser menor que 0K");
+            throw new IllegalArgumentException("A temperatura nao pode ser menor que 0K"); //Se nao: lanca erro.
         }
         this.temperatura_atual = nova_temperatura; //Se for: seta o valor.
 
-        return nova_temperatura; //Retorna nova temperatura.
+        return nova_temperatura; 
     }
 
+
+    /**
+     * Altera a pressao atual percebida pelo robo.
+     * @param nova_pressao O novo valor da variavel pressao.
+     */
     protected int set_pressao(int nova_pressao){
-        this.pressao_atual = nova_pressao; //Seta nova pressao.
-        return nova_pressao; //Retorna nova_pressao.
+        this.pressao_atual = nova_pressao; 
+        return nova_pressao; 
     }
 
-    protected int set_velocidade(int nova_velocidade){
-        if(nova_velocidade > this.velocidade_max){
-            throw new IllegalArgumentException("A velocidade maxima nao deve exceder "+this.velocidade_max+"m/s.");
+
+    /**
+     * Altera a velocidade atual do robo.
+     * @param nova_velocidade O novo valor da velocidade.
+     */
+    protected int set_velocidade(int nova_velocidade){ 
+        if(nova_velocidade > this.velocidade_max){ //Verifica se nova_velocidade e permitida dentros dos limites.
+            throw new IllegalArgumentException("A velocidade maxima nao deve exceder "+this.velocidade_max+"m/s."); //Se nao: gera erro.
         }
-        this.velocidade_atual = nova_velocidade;
+        this.velocidade_atual = nova_velocidade; //Se sim: seta velocidade.
         return nova_velocidade;
     }
 
 
+    /**
+     * Retorna temperatura atual percebida pelo robo.
+     */
     protected int get_temperatura(){
-        return this.temperatura_atual;
+        return this.temperatura_atual; 
     }
 
+
+     /**
+     * Retorna pressao atual percebida pelo robo.
+     */
     protected int get_pressao(){
         return this.pressao_atual;
     }
 
+
+     /**
+     * Retorna velocidade atual do robo.
+     */
     protected int get_velocidade(){
         return this.velocidade_atual;
     }
+
+
+     /**
+     * Retorna o planeta sendo explorado pelo robo.
+     */
     protected String get_planeta(){
         return this.planeta_atual;
     }
+
+
+     /**
+     * Retorna se o robo esta sendo usado.
+     */
     protected boolean status_missao(){
         return this.em_missao;
     }
+
+
+     /**
+     * Finaliza a missao e libera o robo para uso.
+     */
     protected void finalizar_exploracao(){
         this.em_missao = false;
         this.planeta_atual = "";
@@ -85,9 +134,6 @@ public class RoboVoadorExplorador extends RoboAereo {
         this.temperatura_atual=0;
         this.velocidade_atual=0;
     }
-
-
-
 }
 
 
