31fc14f905066fd7766d7fda4c0136fa9524324a
Guilherme Henrique da Silva <guilhermehenriquefire@gmail.com>
2025-03-22 13:09:48 -0300

    adição da direção e colodado em um package

---
+++ lab02/src/robo/standart/Robo.java
@@ -0,0 +1,47 @@
+package robo.standart;
+
+public class Robo
+{
+    String nome;
+    private int posicaoX;
+    private int posicaoY;
+    /**
+     * NORTE, SUL, LESTE, OESTE
+     */
+    protected String direcao;
+
+    /**
+     * Construtor da classe Robo.
+     */
+    public Robo(String nome, int posicaoX, int posicaoY, String direcao) {
+        this.nome = nome;
+        this.posicaoX = posicaoX;
+        this.posicaoY = posicaoY;
+        this.direcao = direcao;
+    }
+
+    /**
+     * Adiciona a variacao das coordenadas no valor da coordenada atual.
+     * @param deltaX
+     * @param deltaY
+     */
+    public void mover(int deltaX, int deltaY) {
+        posicaoX += deltaX;
+        posicaoY += deltaY;
+    }
+
+    /**
+     * @return vetor com duas posicoes, que sao (x,y) do robo.
+     */
+    public int[] exibirPosicao(){
+        return new int[]{this.posicaoX, this.posicaoY};
+    }
+
+    /**
+     * @return direção do robo
+     */
+    public String getDirecao() {
+        return direcao;
+    }
+
+}
