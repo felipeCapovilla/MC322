4c82863dee6b0cdca864651120bd097c3b9db5ec
Guilherme Henrique da Silva <guilhermehenriquefire@gmail.com>
2025-04-22 15:44:26 -0300

    monitorar vertical (altura)

--- lab03/src/sensor/espacial/SensorEspacial.java
+++ lab03/src/sensor/espacial/SensorEspacial.java
@@ -12,6 +12,13 @@ public class SensorEspacial extends Sensor{
         super(raio_alcance, modelo);
     }
 
+    /**
+     * Sensor verifica uma area quadrada no plano de distancia Raio_alcance do sensor 
+     * @param ambiente ambiente que o sensor está inserido
+     * @param X posição X do sensor
+     * @param Y posição Y do sensor
+     * @param Z posição Z do sensor
+     */
     public void monitorarPlano(Ambiente ambiente, int X, int Y, int Z){
         int rightSpace, leftSpace, upSpace, downSpace;
 
@@ -45,6 +52,7 @@ public class SensorEspacial extends Sensor{
 
 
         //Imprimir
+        System.out.println("Plano:");
         for(int currentY = Y + upSpace; currentY >= Y - downSpace; currentY--){
             for(int currentX = X - leftSpace; currentX <= X + rightSpace; currentX++){
                 if(currentX == X & currentY == Y){
@@ -76,16 +84,64 @@ public class SensorEspacial extends Sensor{
             }
             System.out.println(""); // Quebrar linha
         }
+    }
 
+    /**
+     * Sensor verifica a vertical (altitude) de distancia Raio_alcance do sensor 
+     * @param ambiente ambiente que o sensor está inserido
+     * @param X posição X do sensor
+     * @param Y posição Y do sensor
+     * @param Z posição Z do sensor
+     */
+    public void monitorarAltura(Ambiente ambiente, int X, int Y, int Z){
+        int upSpace, downSpace;
 
+        //Acima do sensor
+        if(get_raioAlcance() + Z > ambiente.getAltura()){
+            upSpace = ambiente.get_comprimento() - Z - 1;
+        } else {
+            upSpace = get_raioAlcance();
+        }
+
+        //Abaixo do sensor
+        if(get_raioAlcance() > Z){
+            downSpace = Z;
+        } else {
+            downSpace = get_raioAlcance();
+        }
+
+        //Imprimir
+        System.out.println("Altitude:");
+        for(int currentZ = Z + upSpace; currentZ>= Z - downSpace; currentZ--){
+            if(currentZ == Z){
+                System.out.println("\tR"); //Posicao do sensor
+            } else{ //arredores do sensor
+                //Vazio
+                char sprite = '.';
+
+                //Verificar robos
+                ArrayList<Robo> listaRobos = ambiente.getListaRobos();
+                for(int i = 0; sprite != '@' && i < listaRobos.size(); i++){
+                    int[] pos = listaRobos.get(i).get_posicao();
+
+                    if(pos[0] == X && pos[1] == Y && listaRobos.get(i).get_altitude() == currentZ){
+                        sprite = '@';
+                    }
+                }
+
+                //Verificar obstáculo
+                ArrayList<Obstaculo> obstaculos = ambiente.getObstaculos();
+                for(int i = 0; sprite != 'X' && sprite != '@' && i < obstaculos.size(); i++){
+                    if(obstaculos.get(i).estaDentro(X, Y) && obstaculos.get(i).getAltura() > currentZ){
+                        sprite = 'X';
+                    }
+                }
+
+                System.out.println("\t" + sprite);
+            }
+        }
+        System.out.println("");
 
 
     }
-    
-    
-    
-
-
-
-
 }
