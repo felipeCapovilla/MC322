2316460670c31a230afc89c77bc9755d9fe0b149
Guilherme Henrique da Silva <guilhermehenriquefire@gmail.com>
2025-05-15 10:56:52 -0300

    adição de sprites

--- lab04/src/constantes/TipoEntidade.java
+++ lab04/src/constantes/TipoEntidade.java
@@ -1,5 +1,18 @@
 package constantes;
 
 public enum TipoEntidade {
-    VAZIO, ROBO, OBSTACULO, DESCONHECIDO
+    VAZIO('.'),
+    ROBO('@'),
+    OBSTACULO('#'),
+    DESCONHECIDO('?');
+
+    char representacao;
+
+    private TipoEntidade(char representacao) {
+        this.representacao = representacao;
+    }
+
+    public char getRepresentacao() {
+        return representacao;
+    }
 }
